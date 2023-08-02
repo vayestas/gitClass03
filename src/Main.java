ee2b0bc8802ddd1bfcb1e8f80f18648d89f8e337
vayestas <vayestas88@gmail.com>
2023-08-02 19:22:21 -0400

    the code that tells me the availability of the product

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean productAvailable=true;
+        if (productAvailable){
+            System.out.println("yes");
+        }else {
+            System.out.println("no");
+        }
+    }
+}
